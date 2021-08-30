baa8f6abcc68c81ebe3045b28207afe6151a8537
antar <toobrokeforrysll@gmail.com>
2021-08-30 11:48:36 +0200

    feat: added Call

--- submit.php
+++ submit.php
@@ -1,5 +1,7 @@
 <?php
 
+$conn = new PDO("mysql:host=127.0.0.1;dbname=M151", "root", "getElementById()");
+
 $firstname =  $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $birthday = $_POST["birthday"];
@@ -9,12 +11,13 @@ $personal = $_POST["personal"];
 $telephone = $_POST["telephone"];
 $company = $_POST["company"];
 $department =  $_POST["department"];
-$jobname = $_POST["jobname"];
+$jobtitle = $_POST["jobtitle"];
 $jobdesc = $_POST["jobdesc"];
 
-$pdo = require_once 'connect.php';
-$stmt = $pdo->prepare("CALL insert(?)");
-$stmt->bindParam(1, $firstname, $lastname, $birthday, $email, $ahv, $personal, $telephone, $company, $department, $jobname, $jobdesc, PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000);
+
+$sql = "CALL insert('$firstname', '$lastname', '$birthday', '$email', '$ahv', '$personal', '$telephone', '$company', '$department', '$jobtitle', '$jobdesc')";
+
+$stmt = $conn->prepare($sql);
 $stmt->execute();
 
 ?>
