9756f69aaadb39007a9a84b88e40e7c5b6e6c0c0
speyck <54800864+speyck@users.noreply.github.com>
2021-09-06 11:43:35 +0200

    Fix antars biggest mistake in his career

--- submit.php
+++ submit.php
@@ -36,39 +36,39 @@ Tools::CallProc($db, "CALL mysql_insert", $array);
 		<h1 class="mb-5 text-center">Welcome to M151!</h1>
 		<form action="submit.php" method="POST">            
 			<label for="firstname">Firstname:</label>
-			<input type="text" class="form-control" id="firstname" name="firstname" required><?php $_POST["firstname"] ?><br>			
+			<input type="text" class="form-control" id="firstname" name="firstname" value="<?= $_POST["firstname"] ?>" required><br>
 
 			<label for="lastname">Lastname:</label>
-			<input type="text" class="form-control" id="lastname" name="lastname" required><?php $_POST["lastname"] ?><br>
-		
-			<label for="birthday">Birthday:</label>
-			<input type="date" class="form-control" id="birthday" name="birthday" required><?php $_POST["birthday"] ?><br>
-		
-			<label for="email">E-Mail:</label>
-			<input type="email" class="form-control" id="email" name="email" required><?php $_POST["email"] ?><br>
-		
-			<label for="ahv">AHV-Nummer:</label>
-			<input type="text" class="form-control" id="ahv" name="ahv" required pattern="^\s*\d{3}.\d{4}.\d{4}.\d{2}\s*$" title="Must match 'www.xxxx.yyyy.zz' pattern"><?php $_POST["ahv"] ?><br>
-		
-			<label for="personal">Personal Number:</label>
-			<input type="number" class="form-control" id="personal" name="personal" required><?php $_POST["personal"] ?><br>
-		
-			<label for="telephone">Telephone:</label>
-			<input type="tel" class="form-control" id="telephone" name="telephone"><?php $_POST["telephone"] ?><br>
-		
-			<label for="company">Company Name:</label>
-			<input type="text" class="form-control" id="company" name="company" required><?php $_POST["company"] ?><br>
-		
-			<label for="department">Department:</label>
-			<input type="text" class="form-control" id="department" name="department" required><?php $_POST["department"] ?><br>
-		
-			<label for="jobtitle">Job Title:</label>
-			<input type="text" class="form-control" id="jobtitle" name="jobtitle" required><?php $_POST["jobtitle"] ?><br>
-		
-			<label for="jobdesc">Job Description:</label>
-			<input type="text" class="form-control" id="jobdesc" name="jobdesc"><?php $_POST["jobdesc"] ?><br>     
+			<input type="text" class="form-control" id="lastname" name="lastname" value="<?= $_POST["lastname"] ?>" required><br>
 
-			<input type="submit" text="Edit" class="btn btn-primary">
+			<label for="birthday">Birthday:</label>
+			<input type="date" class="form-control" id="birthday" name="birthday" value="<?= $_POST["birthday"] ?>" required><br>
+
+			<label for="email">E-Mail:</label>
+			<input type="email" class="form-control" id="email" name="email" value="<?= $_POST["email"] ?>"><br>
+
+			<label for="ahv">AHV-Nummer:</label>
+			<input type="text" class="form-control" id="ahv" name="ahv" value="<?= $_POST["ahv"] ?>" required pattern="^\s*\d{3}.\d{4}.\d{4}.\d{2}\s*$" title="Must match 'www.xxxx.yyyy.zz' pattern"><br>
+
+			<label for="personal">Personal Number:</label>
+			<input type="number" class="form-control" id="personal" value="<?= $_POST["personal"] ?>" name="personal" required><br>
+
+			<label for="telephone">Telephone:</label>
+			<input type="tel" class="form-control" id="telephone" value="<?= $_POST["telephone"] ?>" name="telephone"><br>
+
+			<label for="company">Company Name:</label>
+			<input type="text" class="form-control" id="company" name="company" value="<?= $_POST["company"] ?>" required><br>
+
+			<label for="department">Department:</label>
+			<input type="text" class="form-control" id="department" name="department" value="<?= $_POST["department"] ?>" required><br>
+
+			<label for="jobtitle">Job Title:</label>
+			<input type="text" class="form-control" id="jobtitle" name="jobtitle" value="<?= $_POST["jobtitle"] ?>" required><br>
+
+			<label for="jobdesc">Job Description:</label>
+			<input type="text" class="form-control" id="jobdesc" name="jobdesc" value="<?= $_POST["jobdesc"] ?>"><br>     
+
+			<input type="submit" value="Edit" class="btn btn-primary">
 		</form>
 </body>
 </html>
